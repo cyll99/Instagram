04ef26f0b98652347891520ef74646939aa81cee
cyll99 <christyanlove@gmail.com>
2022-08-31 18:24:21 -0400

    fourt video

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     EditText description;
     Button btnTakePicture;
@@ -19,8 +27,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        description = findViewById(R.id.description);
+        btnTakePicture = findViewById(R.id.btnCapture);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        image = findViewById(R.id.image);
 
+        queryPost();
+    }
 
+    private void queryPost() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG,"issue findind post", e);
+                    return;
+                }
+                for (Post post: posts){
+                    Log.i(TAG, "Post: "+ post.getDescription() + " user: " +post.getUser().getUsername());
+                }
+            }
+        });
 
     }
 }
